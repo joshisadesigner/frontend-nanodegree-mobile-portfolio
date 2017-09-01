0395dbbd15243fad52d6faf480ce8f2b4c83db8d
Joshisadesigner <joshisadesigner@gmail.com>
2017-08-31 23:38:48 -0600

    Configure source direcotries in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -30,10 +30,10 @@ gulp.task( 'styles', function(){
 } );
 
 gulp.task( 'useref', function(){
-    return gulp.src( 'app/*.html' )
+    return gulp.src( 'app/**/*.html' )
         .pipe( useref() )
-        .pipe( gulpIf( '*/.js', uglify() ) )
-        .pipe( gulpIf( '*/.css', cssnano() ) )
+        .pipe( gulpIf( '**/*/.js', uglify() ) )
+        .pipe( gulpIf( '**/*/.css', cssnano() ) )
         .pipe( gulp.dest( 'dist' ));
 } );
 
